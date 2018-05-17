7989248b1f48ebe35b10c57b26be945cbe087d0f
yiplee <guoyinl@gmail.com>
2018-05-18 00:06:10 +0800

    foxone wechat bot

--- mybot.js
+++ mybot.js
@@ -1,9 +1,9 @@
 // import { Wechaty, Room, Contact, config, Message } from "wechaty"
 const { Wechaty, Room ,Contact, config, Message, MsgType} = require('wechaty')
+const axios = require('axios')
 
 bot = Wechaty.instance({ profile: config.default.DEFAULT_PROFILE })
-
-var open = require('amqplib').connect('amqp://localhost');
+const bot_config = require('config.json')
 
 bot
 	.on('scan', (url, code) => {
@@ -15,73 +15,33 @@ bot
 
 	.on('login', (user) => {
         console.log(`${user} login`)
-
-        open.then(function(conn) {
-            return conn.createChannel();
-          }).then(function(ch) {
-            let q = "wechat_send"
-            return ch.assertQueue(q,{durable: false}).then(function(ok) {
-              return ch.consume(q, async function(msg) {
-                if (msg !== null) {
-                    let obj = JSON.parse(msg.content.toString())
-                    let content = obj.msg
-                    let from = obj.from
-                    let room = obj.room
-
-                    if (content) {
-                        if (room) {
-                            let receiver = await Room.find({topic:room})
-                            if (receiver) {
-                                receiver.say(content)
-                            }
-                        } else if (from) {
-                            let receiver = await Contact.find({name:from})
-                            if (receiver) {
-                                receiver.say(content)
-                            }
-                        } 
-                    }
-                    ch.ack(msg)
-                }
-              });
-            });
-          }).catch(console.warn);
 	})
 
-	.on('friend', async function (contact, request) {
-		if (request) {
-			await request.accept()
-		}
-	})
+	// .on('friend', async function (contact, request) {
+	// 	if (request) {
+	// 		await request.accept()
+	// 	}
+	// })
 
 	.on('message', (m) => {
 		const contact = m.from()
 		const content = m.content()
 		const room = m.room()
 
-		if (m.self() || m.type() != MsgType.TEXT) {
+		if (m.type() != MsgType.TEXT) {
 			return
         }
         
-        const payload = {
-            "msg":content,
-            "from":contact.name(),
-        }
-
         if (room) {
-            payload.room = room.topic()
+            const payload = {
+                "message_content" : content,
+                "nickname"        : contact.name(),
+                "group_name"      : room.topic(),
+                "timestamp"       : Date.now()
+            }
+
+            axios.post(bot_config.url, payload)
         }
-
-        message = JSON.stringify(payload)
-
-        open.then(function(conn) {
-            return conn.createChannel()
-        }).then(function(ch) {
-            let q = "wechat_get"
-            return ch.assertQueue(q,{durable: false}).then(function(ok) {
-              return ch.sendToQueue(q, new Buffer.from(message,'utf-8'))
-            })
-        }).catch(console.warn)
 	})
 
     .start()
